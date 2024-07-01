e88a6d9860cfb7dfa143a70644318f5117c824e0
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:35:47 +0200

    Problems: Number 1 solved, fast

--- problems/1/main.cpp
+++ problems/1/main.cpp
@@ -12,7 +12,23 @@ void printContainer(const _C & container) {
 
 class Solution {
 public:
-    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // map with already checked numbers and their index
+        unordered_map<int, int> found;
+
+        for (int i = 0; i < nums.size(); ++i) {
+
+            int num = nums[i];
+            // Trying to find oposite number to num in found
+            auto it = found.find(target - num);
+
+            if (it != found.end()) {
+                return {i, it->second};
+            }
+            found[num] = i;
+        }
+        return {};
+    }
 };
 
 
@@ -21,5 +37,37 @@ public:
 int main() {
 
     Solution s;
+    vector<int> res;
 
+    vector<int> nums1 = {2,7,11,15};
+    int target1 = 9;
+    vector<int> res1 = {0, 1};
+    res = s.twoSum(nums1, target1);
+
+    std::sort(res.begin(), res.end());
+    std::sort(res1.begin(), res1.end());
+
+    assert(res == res1);
+
+
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> res2 = {1, 2};
+    res = s.twoSum(nums2, target2);
+
+    std::sort(res.begin(), res.end());
+    std::sort(res2.begin(), res2.end());
+
+    assert(res == res2);
+
+
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> res3 = {0, 1};
+    res = s.twoSum(nums3, target3);
+
+    std::sort(res.begin(), res.end());
+    std::sort(res3.begin(), res3.end());
+
+    assert(res == res3);
 }
