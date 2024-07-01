d9ae9a42871548af9a9c5b14f0b01ff9c1aaf14b
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-01 15:06:34 +0200

    Problems: Number 16, fast

--- problems/16/main.cpp
+++ problems/16/main.cpp
@@ -14,7 +14,32 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int threeSumClosest(vector<int>& nums, int target) {
+      int best = INT_MAX / 2;
+      int res;
+
+      std::sort(nums.begin(), nums.end());
+
+      for (int i = 0; i < nums.size() - 2; i++) {
+        int l = i + 1, h = nums.size() - 1;
+
+        while (l < h) {
+          res = nums[i] + nums[l] + nums[h];
+
+          if (abs(target - res) < abs(target - best))
+            best = res;
+          
+          if (res > target) {
+            h--;
+          } else if (res < target) {
+            l++;
+          } else {
+            return target;
+          }
+        }
+      }
+      return best;
+    }
 };
 
 
