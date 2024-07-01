5b97e0f6e5a1ade8c08a809ef68d914d9af73d93
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:53:53 +0200

    Problems: Number 5, fast

--- problems/5/main.cpp
+++ problems/5/main.cpp
@@ -14,7 +14,29 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    string longestPalindrome(string s) {
+      int start = 0, end = 0;
+
+      for (size_t i = 0; i < 2 * s.size() - 1; i++) {
+        int st = i / 2, ed = i / 2 + (i % 2 == 1);
+
+        if (s[st] != s[ed])
+          continue;
+
+        while (st > 0 && ed < s.size() - 1 && s[st - 1] == s[ed + 1]) {
+          st--; ed++;
+        }
+        if (ed - st > end - start) {
+          start = st;
+          end = ed;
+        }
+      }
+      string ret;
+      for (size_t i = start; i <= end; i++) {
+        ret.push_back(s[i]);
+      }
+      return ret;
+    }
 };
 
 
