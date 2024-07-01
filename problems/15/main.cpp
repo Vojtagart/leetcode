c443ddecc16cf55e9ec3230f1a94645611d5d32a
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-01 13:48:52 +0200

    Problems: Number 15, fast

--- problems/15/main.cpp
+++ problems/15/main.cpp
@@ -14,7 +14,40 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      std::sort(nums.begin(), nums.end());
+      vector<vector<int>> ret;
+        
+      for (int i = 0; i < nums.size() - 2; i++) {
+        int j = i + 1, k = nums.size() - 1;
+
+        if (nums[i] > 0)
+          break;
+        if (i > 0 && nums[i] == nums[i - 1])
+          continue;
+
+        while (j < k) {
+          int res = nums[i] + nums[j] + nums[k];
+
+          if (res > 0) {
+            k--;
+          } else if (res < 0) {
+            j++;
+          } else {
+            ret.push_back({nums[i], nums[j], nums[k]});
+            int pj = nums[j], pk = nums[k];
+
+            while (j < k && pj == nums[j]) {
+              j++;
+            }
+            while (j < k && pk == nums[k]) {
+              k--;
+            }
+          }
+        }
+      }
+      return ret;
+    }
 };
 
 
