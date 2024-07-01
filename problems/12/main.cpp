8eb2889d151f38809cac69aea02a47dbc422c048
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 12:03:59 +0200

    Problems: Number 12, fast and simple

--- problems/12/main.cpp
+++ problems/12/main.cpp
@@ -14,7 +14,22 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    string intToRoman(int num) {
+
+      pair<int, string> vals [] = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"},
+        {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+      };
+      string ret;
+
+      for (int i = 0; i < 13; i++) {
+        while (num >= vals[i].first) {
+          num -= vals[i].first;
+          ret += vals[i].second;
+        }
+      }
+      return ret;
+    }
 };
 
 
