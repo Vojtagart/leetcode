65dc383ab531652bf6ea0635894295da0db6e676
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:56:21 +0200

    Problems: Number 7, fast

--- problems/7/main.cpp
+++ problems/7/main.cpp
@@ -14,7 +14,22 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int reverse(int x) {
+
+      int res = 0;
+      bool isPos = (x >= 0);
+
+      while (x != 0) {
+
+        if (isPos ? res > INT_MAX / 10 : res < INT_MIN / 10) {
+          return 0;
+        }
+        res *= 10;
+        res += x % 10;
+        x /= 10;
+      }
+      return res;
+    }
 };
 
 
