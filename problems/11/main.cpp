735a9fda3b2c2706a1dbc58c4c952bba3079659a
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 12:02:33 +0200

    Problems: Number 11, fast, I guess

--- problems/11/main.cpp
+++ problems/11/main.cpp
@@ -14,7 +14,27 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int maxArea(vector<int>& height) {
+      int max = 0;
+      int st = 0, ed = height.size() - 1;
+
+      while (st < ed) {
+
+        if (max != 0 && max / (ed - st) == 0)
+          break;
+
+        int ar = (ed - st) * (std::min(height[st], height[ed]));
+        max = std::max(ar, max);
+
+        if (height[st] <= height[ed]) {
+          st++;
+        }
+        else {
+          ed--;
+        }
+      }
+      return max;
+    }
 };
 
 
