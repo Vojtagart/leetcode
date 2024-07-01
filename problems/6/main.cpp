a41debeb18b39381db7e2346dd2647b4351c2301
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:55:22 +0200

    Problems: Number 6, fast

--- problems/6/main.cpp
+++ problems/6/main.cpp
@@ -14,7 +14,32 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    string convert(string s, int numRows) {
+      if (numRows <= 1) {
+        return s;
+      }
+      string res;
+      res.reserve(s.size() + 1);
+      int ofs = 2 * numRows - 2;
+
+      for (int row = 0; row < numRows && row < s.size(); row++) {
+
+        int idx1 = row, idx2 = ofs - row;
+
+        while (idx1 < s.size()) {
+
+          res.push_back(s[idx1]);
+
+          if (row != 0 && idx2 < s.size() && row != numRows - 1) {
+            res.push_back(s[idx2]);
+          }
+          idx1 += ofs;
+          idx2 += ofs;
+
+        }
+      }
+      return res;
+    }
 };
 
 
