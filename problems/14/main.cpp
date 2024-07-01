60e7df870cb2c0d315d66e43bf9f843d5da3cf52
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 12:06:44 +0200

    Problems: Number 14, fast

--- problems/14/main.cpp
+++ problems/14/main.cpp
@@ -14,7 +14,25 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    string longestCommonPrefix(vector<string>& strs) {
+
+    if (strs.size() == 0)
+      return "";
+
+    string ret;
+        
+    for (int i = 0; i < strs[0].size(); i++) {
+
+      char c = strs[0][i];
+
+      for (const auto & x : strs) {
+        if (x.size() < i || x[i] != c)
+          return ret;
+        }
+        ret.push_back(c);
+      }
+      return ret;
+    }
 };
 
 
