7363b91c217a3acfd48212af6792cb5e531f2357
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:57:37 +0200

    Problems: Number 8, fast

--- problems/8/main.cpp
+++ problems/8/main.cpp
@@ -14,7 +14,36 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int myAtoi(string s) {
+
+      if (s.size() == 0) {
+        return 0;
+      }
+      int idx = 0;
+
+      while (idx < s.size() && isspace(s[idx])) {
+        idx++;
+      }
+      bool sign = true;
+
+      if (idx < s.size() && s[idx] == '+') {
+        idx++;
+      } else if (idx < s.size() && s[idx] == '-') {
+        idx++;
+        sign = false;
+      }
+      int res = 0;
+
+      while (idx < s.size() && isdigit(s[idx])) {
+        if (sign ? res > (INT_MAX - (s[idx] - '0')) / 10 : res < (INT_MIN + (s[idx] - '0')) / 10) {
+          return sign ? INT_MAX : INT_MIN;
+        }
+        res *= 10;
+        res += (s[idx] - '0') * (sign ? 1 : -1);
+        idx++;
+      }
+      return res;
+    }
 };
 
 
