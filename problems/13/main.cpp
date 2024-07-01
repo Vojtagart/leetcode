2c3f1d9f105b809d8295270cf73d52bda3e579c0
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 12:05:19 +0200

    Problems: Number 13, fast and simple

--- problems/13/main.cpp
+++ problems/13/main.cpp
@@ -14,7 +14,27 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int romanToInt(string s) {
+        
+      int idx = 0;
+      int prev = 0, cur;
+      int res = 0;
+
+      for (int i = s.size() - 1; i >= 0; i--) {
+        switch (s[i]) {
+          case 'M' : cur = 1000; break;
+          case 'D' : cur = 500; break;
+          case 'C' : cur = 100; break;
+          case 'L' : cur = 50; break;
+          case 'X' : cur = 10; break;
+          case 'V' : cur = 5; break;
+          case 'I' : cur = 1; break;
+        }
+        res += (prev <= cur ? cur : -cur);
+        prev = cur;
+      }
+      return res;
+    }
 };
 
 
