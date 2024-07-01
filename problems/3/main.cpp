fae0b0004e06fc28e9234bfd7e03c4e341d8cb81
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:50:12 +0200

    Problems: Number 3, fast

--- problems/3/main.cpp
+++ problems/3/main.cpp
@@ -14,7 +14,22 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int lengthOfLongestSubstring(string s) {
+      int max = 0;
+      int st = 0, ed = 0;
+
+      while (ed < s.size()) {
+        for (size_t i = st; i < ed; i++) {
+          if (s[i] == s[ed]) {
+            st = i + 1;
+            break;
+          }
+        }
+        max = max < ed - st + 1 ? ed - st + 1 : max;
+        ed += 1;
+      }
+    return max;
+    }
 };
 
 
