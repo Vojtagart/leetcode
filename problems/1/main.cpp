8dae23f9063706acb290cabc3e8fd4b6f5c0d6e6
brozvoj2 <brozvoj2@fit.cvut.cz>
2024-07-01 11:27:13 +0200

    Problems: Add basic layout for the first 100 problems

---
+++ problems/1/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename _C>
+void printContainer(const _C & container) {
+    for (const auto & x : container) {
+        cout << x << endl;
+    }
+}
+
+
+class Solution {
+public:
+    
+};
+
+
+
+
+int main() {
+
+    Solution s;
+
+}
