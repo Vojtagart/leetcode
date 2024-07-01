b8733a3bdd7bc61cdc440cbbb1eb2dc2cb4c53f7
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-01 15:34:28 +0200

    Problems: Number 17, fast iterative

--- problems/17/main.cpp
+++ problems/17/main.cpp
@@ -14,7 +14,31 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<string> letterCombinations(string digits) {
+
+      if (digits.size() == 0)
+        return {};
+      vector<string> res = {""};
+
+      for (int i = 0; i < digits.size(); i++) {
+        vector<string> newRes;
+
+        for (char c : m_vals[digits[i] - '0' - 2]) {
+          for (const string & str : res) {
+            newRes.emplace_back(string(str + c));
+          }
+        }
+        res = newRes;
+      }
+      return res;
+    }
+  private:
+    static const vector<char> m_vals [];
+};
+
+const vector<char> Solution::m_vals [] = {
+  {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'},
+  {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}
 };
 
 
@@ -23,5 +47,9 @@ class Solution {
 int main() {
 
   Solution s;
+  string str = "23";
+
+  auto res = s.letterCombinations(str);
+  printContainer(res);
     
 }
