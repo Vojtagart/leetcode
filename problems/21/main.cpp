c20c545af0c2be4a79fe21507b4a327a5d874a43
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-02 16:13:21 +0200

    Problems: Number 21, ugly

--- problems/21/main.cpp
+++ problems/21/main.cpp
@@ -12,9 +12,41 @@ void printContainer(const _C & container) {
 //=====================================================================
 
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
 class Solution {
   public:
-    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+      ListNode * root = nullptr, * p1 = list1, * p2 = list2, * ptr;
+
+      while (p1 || p2) {
+        if ((p2 == nullptr) || (p1 != nullptr && p1->val <= p2->val)) {
+          if (root == nullptr) {
+            ptr = root = new ListNode(p1->val);
+          } else {
+            ptr->next = new ListNode(p1->val);
+            ptr = ptr->next;
+          }
+          p1 = p1->next;
+        } else {
+          if (root == nullptr) {
+            ptr = root = new ListNode(p2->val);
+          } else {
+            ptr->next = new ListNode(p2->val);
+            ptr = ptr->next;
+          }
+          p2 = p2->next;
+        }
+      }
+      return root;
+    }
 };
 
 
