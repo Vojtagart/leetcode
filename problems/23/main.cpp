92197d7ffb73ac171e685a13cb29a0c6534676d6
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-02 18:06:30 +0200

    Problems: Number 23, fast

--- problems/23/main.cpp
+++ problems/23/main.cpp
@@ -12,9 +12,42 @@ void printContainer(const _C & container) {
 //=====================================================================
 
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
 class Solution {
   public:
-    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+      ListNode * root = new ListNode();
+      ListNode * ptr = root;
+
+      auto cmp = [](const ListNode * x, const ListNode * y) -> bool {return x->val > y->val;};
+      priority_queue<ListNode *, vector<ListNode*>, decltype(cmp)> q;
+
+      for (const auto & x : lists) {
+        if (x != nullptr)
+          q.push(x);
+      }
+
+      while (!q.empty()) {
+        ptr->next = q.top();
+        q.pop();
+
+        ptr = ptr->next;
+
+        if (ptr->next != nullptr)
+          q.push(ptr->next);
+      }
+      auto ret = root->next;
+      delete root;
+      return ret;
+    }
 };
 
 
