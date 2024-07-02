cd100ebbb5a7b4b7d2e08323fd930385e87cd17d
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-02 22:04:50 +0200

    Problems: Number 24, fast

--- problems/24/main.cpp
+++ problems/24/main.cpp
@@ -12,9 +12,35 @@ void printContainer(const _C & container) {
 //=====================================================================
 
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
 class Solution {
   public:
-    
+    ListNode* swapPairs(ListNode* head) {
+      ListNode * ptr = head;
+
+      if (ptr && ptr->next) {
+        head = ptr->next;
+        ListNode * tmp = head->next;
+        head->next = ptr;
+        ptr->next = tmp;
+      }
+      while (ptr && ptr->next && ptr->next->next) {
+        ListNode * first = ptr->next->next;
+        ptr->next->next = first->next;
+        first->next = ptr->next;
+        ptr->next = first;
+        ptr = ptr->next->next;
+      }
+      return head;
+    }
 };
 
 
