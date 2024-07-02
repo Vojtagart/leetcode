d569c90ac5adb8e9259812f7c008bccfa301e033
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-02 13:43:13 +0200

    Problems: Number 19, fast

--- problems/19/main.cpp
+++ problems/19/main.cpp
@@ -12,9 +12,38 @@ void printContainer(const _C & container) {
 //=====================================================================
 
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
 class Solution {
   public:
-    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      
+      int len = 0;
+      for (ListNode * ptr = head; ptr; ptr = ptr->next) {
+        len++;
+      }
+      if (len - n == 0) {
+        ListNode * ret = head->next;
+        delete head;
+        return ret;
+      }
+      ListNode * ptr = head;
+      for (int i = 1; i < len - n; i++) {
+        ptr = ptr->next;
+      }
+      ListNode * next = ptr->next->next;
+      delete ptr->next;
+      ptr->next = next;
+
+      return head;
+    }
 };
 
 
