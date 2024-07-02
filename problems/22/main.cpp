ee15a061371be52bd2ffb2e647098c87aa7aa846
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-02 17:24:33 +0200

    Problems: Number 22, easy recursion

--- problems/22/main.cpp
+++ problems/22/main.cpp
@@ -14,7 +14,25 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<string> generateParenthesis(int n) {
+      vector<string> res;
+
+      rec("", 0, n, res);
+
+      return res;
+    }
+    void rec(const string & str, int open, int n, vector<string> & res) {
+      if (str.size() == n * 2) {
+        res.push_back(str);
+      } else {
+        if ((str.size() - open) / 2 + open < n) {
+          rec(str + "(", open + 1, n, res);
+        }
+        if (open > 0) {
+          rec(str + ")", open - 1, n, res);
+        }
+      }
+    }
 };
 
 
@@ -23,5 +41,8 @@ class Solution {
 int main() {
 
   Solution s;
+  auto res = s.generateParenthesis(3);
+
+  printContainer(res);
     
 }
