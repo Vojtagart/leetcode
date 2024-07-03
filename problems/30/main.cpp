75c17fc6c25476816bc5b8d5fbf370793aca806f
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 17:28:54 +0200

    Problems: Number 30, fast (for high frequencies in words)

--- problems/30/main.cpp
+++ problems/30/main.cpp
@@ -14,7 +14,62 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+      vector<int> ret;
+      wlen = words[0].size();
+      clen = wlen * words.size();
+      getFreq(words);
+
+      for (int i = 0; i < words[0].size(); i++) {
+        solveFrom(i, s, ret);
+      }
+      return ret;
+    }
+
+    void solveFrom(int l, const string & s, vector<int> & ret) {
+
+      int r = l;
+      unordered_map<string, queue<int>> seen;
+
+      while (l <= static_cast<int>(s.size()) - clen) {
+
+        string str = s.substr(r, wlen);
+        auto p = freq.find(str);
+
+        if (p == freq.end()) {
+          l = r + wlen; r = l;
+          continue;
+        }
+        auto it = seen.insert({str, queue<int>()}).first;
+        it->second.push(r);
+
+        if (it->second.size() > p->second) {
+          l = max(l, it->second.front() + wlen);
+          it->second.pop();
+        }
+        r += wlen;
+
+        if (r - l == clen) {
+          ret.push_back(l);
+          l += wlen;
+        }
+      }
+    }
+
+    void getFreq(const vector<string> & words) {
+
+      for (const auto & x : words) {
+        auto p = freq.insert({x, 1});
+        if (!p.second)
+          p.first->second += 1;
+      }
+    }
+
+  private:
+    unordered_map<string, int> freq;
+    int wlen;
+    int clen;
 };
 
 
@@ -23,5 +78,10 @@ class Solution {
 int main() {
 
   Solution s;
-    
+  string str = "barfoofoobarthefoobarman";
+  vector<string> vec = {"bar","foo","the"};
+  auto res = s.findSubstring(str, vec);
+  
+  cout << "Result:" << endl;
+  printContainer(res);
 }
