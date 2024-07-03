59cfb0ceeb939fd08ec007445d5261399565721d
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 20:45:05 +0200

    Problems: Number 32, not the best, but beats 100%

--- problems/32/main.cpp
+++ problems/32/main.cpp
@@ -14,7 +14,31 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int longestValidParentheses(string s) {
+      stack<int> st;
+      vector<bool> v(s.size(), false);
+
+      for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+          st.push(i);
+        } else if (!st.empty()) {
+          v[i] = true;
+          v[st.top()] = true;
+          st.pop();
+        }
+      }
+      int max = 0;
+      int cnt = 0;
+      for (bool x : v) {
+        if (x == true) {
+          cnt++;
+        } else {
+          max = std::max(max, cnt);
+          cnt = 0;
+        }
+      }
+      return max > cnt ? max : cnt;
+    }
 };
 
 
@@ -23,5 +47,8 @@ class Solution {
 int main() {
 
   Solution s;
+  string str = "()(()";
+
+  cout << s.longestValidParentheses(str) << endl;
     
 }
