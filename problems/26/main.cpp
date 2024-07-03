56c782be993350f9a2cc257e5a13c67bf240d75a
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 13:03:54 +0200

    Problems: Number 26, piss easy

--- problems/26/main.cpp
+++ problems/26/main.cpp
@@ -14,7 +14,21 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int removeDuplicates(vector<int>& nums) {
+
+      int idx = 0;
+      int prev = INT_MIN;
+
+      for (int i = 0; i < nums.size(); i++) {
+        int cur = nums[i];
+        nums[idx] = nums[i];
+
+        if (cur != prev)
+          idx++;
+        prev = cur;
+      }
+      return idx;
+    }
 };
 
 
