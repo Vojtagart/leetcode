1532d057f0efcf6f37042703cbbfc33a3b58e3bd
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 12:57:54 +0200

    Problems: Number 25, fast

--- problems/25/main.cpp
+++ problems/25/main.cpp
@@ -12,11 +12,51 @@ void printContainer(const _C & container) {
 //=====================================================================
 
 
-class Solution {
-  public:
-    
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+
+class Solution {
+  public:
+    ListNode * reverseKGroup(ListNode* head, int k) {
+      ListNode * dummy = new ListNode(0, head);
+      ListNode * ptr = dummy;
+
+      if (k == 1)
+        return head;
+      
+      while (true) {
+
+        ListNode * check = ptr;
+
+        for (int i = 0; i < k; i++) {
+          if (!check->next)
+            return dummy->next;
+          check = check->next;
+        }
+        ListNode * prev = nullptr;
+        ListNode * cur = ptr->next;
+        ListNode * next = nullptr;
+        ListNode * first = cur;
+
+        for (int i = 0; i < k; i++) {
+          next = cur->next;
+          cur->next = prev;
+          prev = cur;
+          cur = next;
+        }
+        ptr->next = prev;
+        first->next = cur;
+        ptr = first;
+      }
+      return dummy->next;
+    }
+};
 
 //=====================================================================
 
