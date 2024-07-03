6f348b154446d705bedcb710e9ee018a5d08e38a
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 19:59:42 +0200

    Problems: Nuber 31, fast

--- problems/31/main.cpp
+++ problems/31/main.cpp
@@ -14,7 +14,30 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    void nextPermutation(vector<int>& nums) {
+      int k = -1;
+
+      for (int i = nums.size() - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+          k = i;
+          break;
+        }
+      }
+      if (k == -1) {
+        std::sort(nums.begin(), nums.end());
+        return;
+      }
+      int min = INT_MAX;
+      int minIdx = -1;
+      for (int i = k + 1; i < nums.size(); i++) {
+        if (nums[i] > nums[k] && nums[i] <= min) {
+          min = nums[i];
+          minIdx = i;
+        }
+      }
+      swap(nums[minIdx], nums[k]);
+      reverse(nums.begin() + k + 1, nums.end());
+    }
 };
 
 
@@ -23,5 +46,9 @@ class Solution {
 int main() {
 
   Solution s;
+  vector<int> vec = {2, 3, 1, 3, 3};
+  s.nextPermutation(vec);
+
+  printContainer(vec);
     
 }
