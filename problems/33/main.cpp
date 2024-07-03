5c9f45c55328dc0f94e460bb66ccc78f255b35ce
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 21:35:29 +0200

    Problems: Number 33, fast

--- problems/33/main.cpp
+++ problems/33/main.cpp
@@ -14,7 +14,29 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int search(vector<int>& nums, int target) {
+      int l = 0;
+      int h = nums.size() - 1;
+
+      while (l <= h) {
+        int mid = (l + h) / 2;
+
+        if (nums[mid] == target) {
+          return mid;
+        } else if (nums[l] <= nums[mid]) {
+          if (target < nums[mid] && target >= nums[l])
+            h = mid - 1;
+          else  
+            l = mid + 1;
+        } else {
+          if (target > nums[mid] && target <= nums[h])
+            l = mid + 1;
+          else  
+            h = mid - 1;
+        }
+      }
+      return -1;
+    }
 };
 
 
