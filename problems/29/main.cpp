f182ea90f431469b653d293fd73eab55b7a373df
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 14:30:38 +0200

    Problems: Number 29, fast

--- problems/29/main.cpp
+++ problems/29/main.cpp
@@ -14,7 +14,27 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int divide(int dividend, int divisor) {
+      int q = 0;
+
+      if (divisor == 1)
+        return dividend;
+      if (divisor == -1)
+        return dividend == INT_MIN ? INT_MAX : -dividend;
+      bool sign = !((dividend >= 0) ^ (divisor >= 0));
+
+      while (dividend == INT_MIN || abs(dividend) >= abs(divisor)) {
+        long long newDiv = divisor, newQ = 1;
+
+        while(abs(newDiv << 1) <= abs(dividend)) {
+          newDiv <<= 1;
+          newQ <<= 1;
+        }
+        sign ? dividend -= newDiv : dividend += newDiv;
+        sign ? q += newQ : q -= newQ;
+      }
+      return q;
+    }
 };
 
 
