87bddf5ba38ccf07124adef39c2b43aef192f32b
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-03 13:13:43 +0200

    Problems: Number 28, easy but not optimised

--- problems/28/main.cpp
+++ problems/28/main.cpp
@@ -14,7 +14,19 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int strStr(string haystack, string needle) {
+      for (int i = 0; i < 1 + static_cast<int>(haystack.size()) - static_cast<int>(needle.size()); i++) {
+        bool broken = false;
+        for (int j = 0; j < needle.size(); j++) {
+          if (haystack[i + j] != needle[j]) {
+            broken = true; break;
+          }
+        }
+        if (!broken)
+          return i;
+      }
+    return -1;
+    }
 };
 
 
