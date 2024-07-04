3f2747b6fc6affcc99b0aa54729cf865d46158c5
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-04 21:56:13 +0200

    Problems: Number 40, recursive solution

--- problems/40/main.cpp
+++ problems/40/main.cpp
@@ -14,7 +14,37 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+      vector<vector<int>> res;
+      vector<int> tmp;
+
+      sort(candidates.begin(), candidates.end());
+      rec(candidates, target, 0, res, tmp);
+
+      return res;
+    }
+    void rec(const vector<int> & cds, int target, int idx, vector<vector<int>> & res, vector<int> & tmp) {
+      if (target == 0) {
+        res.push_back(tmp);
+        return;
+      }
+      if (target < 0)
+        return;
+
+      int prev = -1;
+      for (int i = idx; i < cds.size(); i++) {
+
+        while (cds[i] == prev) {
+          i++;
+          if (i == cds.size())
+            return;
+        }
+        prev = cds[i];
+        tmp.push_back(cds[i]);
+        rec(cds, target - cds[i], i + 1, res, tmp);
+        tmp.pop_back();
+      }
+    }
 };
 
 
