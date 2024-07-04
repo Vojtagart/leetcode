2295173a36455e4188c199ecccb776d21942d11b
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-04 11:20:16 +0200

    Problems: Number 35, fast

--- problems/35/main.cpp
+++ problems/35/main.cpp
@@ -14,7 +14,22 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int searchInsert(vector<int>& nums, int target) {
+      int l = 0, h = nums.size() - 1, m = -1;
+
+      while (l <= h) {
+        m = (l + h) / 2;
+
+        if (nums[m] < target) {
+          l = m + 1;
+        } else if (nums[m] > target) {
+          h = m - 1;
+        } else {
+          return m;
+        }
+      }
+      return target < nums[m] ? m : m + 1;
+    }
 };
 
 
@@ -23,5 +38,7 @@ class Solution {
 int main() {
 
   Solution s;
-    
+  vector<int> vec = {1,3,5,6};
+  int target = 4;
+  cout << s.searchInsert(vec, target) << endl;
 }
