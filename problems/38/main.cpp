11d7721ebd03d96c56ea33e30e8c17273ad1f6da
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-04 15:53:59 +0200

    Problems: Number 38, fast iterative

--- problems/38/main.cpp
+++ problems/38/main.cpp
@@ -14,7 +14,31 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    string countAndSay(int n) {
+      
+      string ret = "1";
+
+      for (int i = 1; i < n; i++) {
+
+        string newRet;
+
+        for (int j = 0; j < ret.size(); j++) {
+
+          char cur = ret[j];
+          int cnt = 1;
+
+          while (j < ret.size() - 1 && ret[j + 1] == cur) {
+            cnt++;
+            j++;
+          }
+
+          newRet.push_back(cnt + '0');
+          newRet.push_back(cur);
+        }
+        ret = newRet;
+      }
+      return ret;
+    }
 };
 
 
