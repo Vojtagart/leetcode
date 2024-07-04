4e16e1c99045ab82e6c1ec0f548ed043480bbf76
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-04 17:01:59 +0200

    Problems: Number 39, fast recursion

--- problems/39/main.cpp
+++ problems/39/main.cpp
@@ -14,7 +14,26 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+      vector<vector<int>> res;
+      vector<int> tmp;
+      rec(candidates, target, 0, res, tmp);
+      return res;
+    }
+    void rec(const vector<int> & cds, int target, int idx, vector<vector<int>> & res, vector<int> & tmp) {
+      if (target == 0) {
+        res.push_back(tmp);
+        return;
+      }
+      if (target < 0)
+        return;
+
+      for (int i = idx; i < cds.size(); i++) {
+        tmp.push_back(cds[i]);
+        rec(cds, target - cds[i], i, res, tmp);
+        tmp.pop_back();
+      }
+    }
 };
 
 
