5d95664b3338f66a5343cfacae6989b209f45161
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-04 11:00:53 +0200

    Problems: Number 34, fast

--- problems/34/main.cpp
+++ problems/34/main.cpp
@@ -14,7 +14,33 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    vector<int> searchRange(vector<int>& nums, int target) {
+      int l = 0, h = nums.size() - 1, m = 0;
+      return {bsearch(nums, target, true), bsearch(nums, target, false)};
+    }
+
+    int bsearch(const vector<int> & nums, int target, bool isLeft) {
+      int l = 0, h = nums.size() - 1;
+      int res = -1;
+
+      while (l <= h) {
+        int m = (l + h) / 2;
+
+        if (nums[m] < target) {
+          l = m + 1;
+        } else if (nums[m] > target) {
+          h = m - 1;
+        } else {
+          res = m;
+          if (isLeft) {
+            h = m - 1;
+          } else {
+            l = m + 1;
+          }
+        }
+      }
+      return res;
+    }
 };
 
 
@@ -23,5 +49,8 @@ class Solution {
 int main() {
 
   Solution s;
+  vector<int> vec = {1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7};
+
+  printContainer(s.searchRange(vec, 7));
     
 }
