811a5eb7accdb20fcac0c55cf66ee8d328c65771
brozvoj2 <brozvoj2@cvut.fit.cz>
2024-07-05 09:23:47 +0200

    Problems: Number 42, fast

--- problems/42/main.cpp
+++ problems/42/main.cpp
@@ -14,7 +14,27 @@ void printContainer(const _C & container) {
 
 class Solution {
   public:
-    
+    int trap(vector<int>& height) {
+      int left[height.size()];
+      int right[height.size()];
+
+      int max = 0;
+      for (int i = 0; i < height.size(); i++) {
+        left[i] = max;
+        max = std::max(height[i], max);
+      }
+      max = 0;
+      for (int i = height.size() - 1; i >= 0; i--) {
+        right[i] = max;
+        max = std::max(height[i], max);
+      }
+      int vol = 0;
+      for (int i = 0; i < height.size(); i++) {
+        int water = std::min(left[i], right[i]) - height[i];
+        vol += (water > 0)  ? water : 0;
+      }
+      return vol;
+    }
 };
 
 
